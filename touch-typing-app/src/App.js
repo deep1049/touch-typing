ef02a63a0e3e3bdaa7d226d590c47b82b2ac549e
deep1049 <103635801+deep1049@users.noreply.github.com>
2023-05-27 13:44:31 +0530

    touch typing

--- touch-typing-app/src/App.js
+++ touch-typing-app/src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import {
+  FormatTime,
+  GetRandomString,
+  calculateStringAccuracy,
+} from "./common/utility";
 
-function App() {
+const App = () => {
+  const [randomStr, setRandomStr] = useState("");
+  const [inputStr, setInputStr] = useState("");
+  const [result, setResult] = useState({
+    accuracy: "NA",
+  });
+  let [time, setTime] = useState(0);
+  let interval = undefined;
+  useEffect(() => {
+    //console.log(`init timer`);
+    interval = setInterval(() => {
+      updateTime();
+    }, 1000);
+
+    return () => {
+      console.log(`clear time interval`);
+      if (interval) {
+        clearInterval(interval);
+      }
+    };
+  }, []);
+  const updateTime = () => {
+    setTime((prev) => {
+      if (prev === 10) {
+        alert("time up");
+        if (interval) {
+          clearInterval(interval);
+        }
+        const btn = document.getElementById("btn-sub");
+        if (btn) {
+          btn.click();
+        }
+      }
+      return prev + 1;
+    });
+  };
+  //
+  useEffect(() => {
+    const str = GetRandomString();
+    setRandomStr(str);
+  }, []);
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setInputStr(value);
+  };
+
+  const computeResult = () => {
+    if (randomStr.length !== inputStr.length || !inputStr.length) {
+      alert("Input string length does not match the sample stringe!");
+      return;
+    }
+    const accuracy = calculateStringAccuracy(
+      randomStr,
+      inputStr,
+      randomStr.length
+    );
+    // console.log(accuracy);
+    setResult((prev) => ({ ...prev, accuracy: `${accuracy}%` }));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="time">
+        <div> {FormatTime(time)}</div>
+        <span>Timer of 5 minutes Started</span>
+      </div>
+      <label className="random-string">{randomStr}</label>
+      <div className="wrap">
+        <input
+          className="input-field"
+          value={inputStr}
+          onChange={handleChange}
+          placeholder="Type here..."
+        />
+        <input
+          id="btn-sub"
+          className="submit-btn"
+          type="button"
+          value="Submit"
+          onClick={computeResult}
+        />
+      </div>
+      <div className="result">
+        <label>
+          Accuracy: <span>{result.accuracy}</span>
+        </label>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ touch-typing-app/src/common/utility.js
@@ -0,0 +1,50 @@
+import moment from "moment";
+
+export const GetRandomString = () => {
+  let stringLength = 30;
+  let alphabetsAllowed = [" ", "a", "s", "d", "f", "j", "k", "l", ";"];
+
+  let generatedString = "";
+  for (let i = 0; i < stringLength; i++) {
+    let currentNumber = GetRandomNumber();
+
+    if (
+      (currentNumber === 0 && i === 0) ||
+      (currentNumber === 0 && i === stringLength - 1)
+    ) {
+      currentNumber = 1;
+    }
+
+    generatedString += alphabetsAllowed[currentNumber];
+  }
+  return generatedString;
+};
+
+const GetRandomNumber = () => {
+  return Math.floor(Math.random() * 8);
+};
+
+export const calculateStringAccuracy = (s1 = "", s2 = "", loopLength) => {
+  let longString = "";
+  let shortString = "";
+  if (s1.length > s2.length) {
+    longString = s1;
+    shortString = s2;
+  } else {
+    longString = s2;
+    shortString = s1;
+  }
+  let matchCount = 0;
+  //   console.log("abc", s1, s2);
+  for (let i = 0; i < loopLength; i++) {
+    if (longString.charAt(i) === shortString.charAt(i)) {
+      ++matchCount;
+    }
+  }
+  let accuracy = (matchCount / longString.length) * 100;
+  return accuracy;
+};
+
+export const FormatTime = (secondsTime) => {
+  return moment.utc(secondsTime * 1000).format("HH:mm:ss");
+};
